ca9f647e65584ac3b05cb83d1d57704e4a431328
vishal <vishalambekar459@gmail.com>
2022-09-23 12:15:19 +0530

    File input stream added

---
+++ Core Java/src/IOStreams/FileInputStreamDemo.java
@@ -0,0 +1,41 @@
+package IOStreams;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamDemo {
+
+	public static void main(String[] args) {
+
+		FileInputStream fis = null;
+		
+		try {
+			fis = new FileInputStream(new File("C:\\Users\\visha\\Desktop\\myfile.txt"));
+			
+			System.out.println("File opened!!!");
+			
+			int i;
+			
+			while((i=fis.read()) != -1) {
+				System.out.print((char)(i));
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("\nclosed file input stream");
+		}
+	}
+
+}

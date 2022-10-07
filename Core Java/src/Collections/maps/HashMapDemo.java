434e322e8798363a124881d5e5b8f640c3a22d1f
vishal <vishalambekar459@gmail.com>
2022-10-07 20:48:50 +0530

    Collections code added

---
+++ Core Java/src/Collections/maps/HashMapDemo.java
@@ -0,0 +1,33 @@
+package Collections.maps;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+
+		Map<String, Integer> map = new HashMap<>();
+		
+		map.put("jhon",70);
+		map.put("tom",73);
+		map.put("Lee",74);
+		map.put("Brad",76);
+		
+		System.out.println("map :: "+map);
+		
+		Set<String> keySet = map.keySet();
+		System.out.println("keySet :: "+keySet);
+		
+		Collection<Integer> values = map.values();
+		System.out.println("values :: "+values);
+		
+		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println("entry :: "+entry+"      entry key :: "+entry.getKey()+"     entry val :: "+entry.getValue());
+		}
+		
+	}
+
+}

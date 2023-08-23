8cb11d40f95afc823805fcdfeddd7e6b6355dde4
vishal <vishalambekar459@gmail.com>
2023-08-24 00:27:23 +0530

    map and flat map demo added

---
+++ Core Java/src/Java8/Stream/playAround/MapAndFlatMapJ8.java
@@ -0,0 +1,46 @@
+package Java8.Stream.playAround;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ComparatorSorting.model.Student;
+
+public class MapAndFlatMapJ8 {
+
+	public static void main(String[] args) {
+
+		List<String> names = Arrays.asList("alex","bay","charle","dave");
+		
+		List<String> res = names.stream()
+		     .map(name -> name.toUpperCase())
+		     .collect(Collectors.toList());
+		
+		System.out.println("res :: "+res);
+		
+		
+		//Example2
+		
+		List<Student> students = new ArrayList<>();
+		students.add(new Student(1, "Alex", "A"));
+		students.add(new Student(2, "Bale", "B"));
+		students.add(new Student(3, "Charles", "C"));
+		
+		List<String> studentNames = students.stream()
+		        .map(s -> s.getFirstName())
+		        .collect(Collectors.toList());
+		
+		System.out.println("student first names :: "+studentNames);
+		
+		
+		//Example of flat map//TODO Reduce operation
+		
+		List<Student> flattenedStudents = students.stream()
+		        .flatMap(student -> students.stream())
+		        .collect(Collectors.toList());
+		
+		System.out.println("flattenedStudents :: "+flattenedStudents);
+	}
+
+}

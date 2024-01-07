8b36ab6fdd3ea49e565e0f1f6b183ea60cd5349a
vishal <vishalambekar459@gmail.com>
2024-01-08 01:07:11 +0530

    Hackers and Police example added for concorrency

--- Core Java/src/concorrency/fundamentals/HackersAndPolice.java
+++ Core Java/src/concorrency/fundamentals/HackersAndPolice.java
@@ -1,33 +1,67 @@
 package concorrency.fundamentals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class HackersAndPolice {
     private static final int MAX = 9999;
 
     public static void main(String[] args) {
+        Random random = new Random();
+        int password = random.nextInt(MAX);
+
+        List<Thread> threads = new ArrayList<>();
+
         Thread hackerThreadAsc = new Thread(() -> {
-            IntStream.range(0,MAX);
+            IntStream.range(0,MAX).forEach(elem -> {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if(elem == password){
+                    System.out.println("Hacked password :: "+password+" by asc team");
+                    System.exit(0);
+                }
+            });
         });
 
         Thread hackerThreadDsc = new Thread(() -> {
-
+            for(int i=MAX;i>=0;i--){
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if(i == password){
+                    System.out.println("Hacked password :: "+password+" by dsc team");
+                    System.exit(0);
+                }
+            }
         });
-    }
-}
 
+        Thread policeThread = new Thread(() -> {
+            IntStream.range(1,11).forEach(elem -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Looking for hackers by police team in :: "+elem);
+            });
+            System.out.println("Caught you hackers");
+            System.exit(0);
+        });
 
-class Vault {
-    private String password;
+        threads.add(hackerThreadAsc);
+        threads.add(hackerThreadDsc);
+        threads.add(policeThread);
 
-    public Vault(String password){
-        this.password = password;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
+        for(Thread t : threads){
+            t.start();
+        }
     }
 }
 
